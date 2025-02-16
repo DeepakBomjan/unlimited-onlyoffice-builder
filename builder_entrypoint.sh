0755b38423b6da53b0811bfbd8d9048f7e58111a
DeepakBomjan <44976635+DeepakBomjan@users.noreply.github.com>
2025-02-16 08:26:50 +0545

    ci: update build

--- builder_entrypoint.sh
+++ builder_entrypoint.sh
@@ -7,19 +7,40 @@ BUILDER_HOME=/root
 ## Prepare build environment
 export TZ=Etc/UTC
 ln -snf /usr/share/zoneinfo/$TZ /etc/localtime && echo $TZ > /etc/timezone
-apt-get -y update && \
-    apt-get -y install python \
-                       python3 \
-                       sudo
-rm /usr/bin/python && ln -s /usr/bin/python2 /usr/bin/python
 
+echo "Updating system and installing dependencies..."
+export DEBIAN_FRONTEND=noninteractive
+apt-get update && apt-get install -y \
+    python3 \
+    python3-pip \
+    git \
+    make \
+    sudo \
+    build-essential \
+    apt-utils \
+    fakeroot \
+    devscripts \
+    equivs
+
+# Ensure python symlink exists
+ln -sf /usr/bin/python3 /usr/bin/python
 
 echo "Building docserver..."
 
-git clone https://github.com/ONLYOFFICE/document-server-package.git
+# Clone required repositories
+if [ ! -d "${BUILDER_HOME}/document-server-package" ]; then
+    git clone https://github.com/ONLYOFFICE/document-server-package.git ${BUILDER_HOME}/document-server-package
+else
+    echo "document-server-package already exists, skipping clone."
+fi
 
-git clone https://github.com/ONLYOFFICE/build_tools.git
+if [ ! -d "${BUILDER_HOME}/build_tools" ]; then
+    git clone https://github.com/ONLYOFFICE/build_tools.git ${BUILDER_HOME}/build_tools
+else
+    echo "build_tools already exists, skipping clone."
+fi
 
+# Modify Makefile
 cd ${BUILDER_HOME}/document-server-package/
 cat << EOF >> Makefile
 
@@ -27,17 +48,15 @@ deb_dependencies: \$(DEB_DEPS)
 
 EOF
 
+# Install Debian dependencies
+echo "Installing Debian dependencies..."
 make deb_dependencies
 
 cd ${BUILDER_HOME}/document-server-package/deb/build/
-
 apt-get -qq build-dep -y ./
 
 cd ${BUILDER_HOME}/document-server-package
-
 make deb
-
-
 
 ## Apply patch
 echo "Applying patches..."
@@ -46,10 +65,7 @@ git apply ${BUILDER_HOME}/patches/build_tools.patch
 
 cd ${BUILDER_HOME}/document-server-package
 
-## Starting building
+## Start building
 echo "Building..."
 cd ${BUILDER_HOME}/build_tools/tools/linux
 python3 ./automate.py server --branch=tags/${PRODUCT_VERSION}.${BUILD_NUMBER}
-
-
-
